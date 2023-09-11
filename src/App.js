0cc59a5c7ed708e127563e99c561abe6240ed5bd
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-09-11 14:35:35 +0900

    20230911-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Tab } from "./components/20230911/Tab";
+import { Session1 } from "./components/20230911/Session1";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Session1 />
+    </>
   );
 }
-
-export default App;
---
+++ src/components/20230911/Session1.js
@@ -0,0 +1,47 @@
+import "./Session1.css";
+
+export function Session1() {
+  return (
+    <>
+      <div class="bigbox1">Hello world</div>
+      <div class="bigbox2">
+        <div class="b1">
+          <img src="https://picsum.photos/id/35/300/200" alt="image" />
+        </div>
+        <div class="b2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis
+          iaculis felis. Nulla nec lectus id arcu tristique fermentum.
+          Pellentesque pretium quam vitae nisl lobortis lacinia. Aliquam aliquam
+          ligula et arcu ullamcorper imperdiet.
+        </div>
+      </div>
+      <div class="bigbox3">
+        <div class="smallbox1">
+          <div>
+            <img src="https://picsum.photos/id/17/300/200" alt="image" />
+          </div>
+          <p>
+            Fusce vitae commodo est, vel faucibus sem. Nullam rutrum viverra
+            justo, eget bibendum ligula tincidunt at.
+          </p>
+        </div>
+        <div class="smallbox2">
+          <div>
+            <img src="https://picsum.photos/id/18/300/200" alt="image" />
+          </div>
+          <p>
+            Vivamus auctor eleifend venenatis. Aenean vulputate, nisi nec
+            hendrerit pellentesque, ante magna fermentum sapien, a mattis tortor
+            dui vel urna.
+          </p>
+        </div>
+        <div class="smallbox3">
+          <div>
+            <img src="https://picsum.photos/id/25/300/200" alt="image" />
+          </div>
+          <p>Ut nec neque nec ligula porttitor malesuada.</p>
+        </div>
+      </div>
+    </>
+  );
+}
