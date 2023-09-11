85c1496afa27ed85c688c08bf5312eb310b559f0
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-09-11 16:17:28 +0900

    20230911-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Tab } from "./components/20230911/Tab";
 import { Session1 } from "./components/20230911/Session1";
+import { List } from "./components/20230911/List";
 
 export default function App() {
   return (
     <>
-      <Session1 />
+      <List />
     </>
   );
 }
