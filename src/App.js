f10f03ccf8e3bc5150d21253edf060002b84a40f
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-09-12 17:27:42 +0900

    20230912-3

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Session1 } from "./components/20230911/Session1";
 import { List } from "./components/20230911/List";
 import { AvatarList } from "./components/20230912/AvatarList";
 import { PropsTest } from "./components/20230912/PropsTest";
+import { CounterState } from "./components/20230912/CounterState";
 
 export default function App() {
   return (
     <>
-      <PropsTest
-        firstName={"철수"}
-        score={{ math: 80, english: 90, history: 100 }}
-      />
+      <CounterState />
     </>
   );
 }
