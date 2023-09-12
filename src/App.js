5515f47752e76d2da3fa9ef6828f2c3fd43deda7
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-09-12 16:09:43 +0900

    20230912-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Tab } from "./components/20230911/Tab";
 import { Session1 } from "./components/20230911/Session1";
 import { List } from "./components/20230911/List";
+import { AvatarList } from "./components/20230912/AvatarList";
+import { PropsTest } from "./components/20230912/PropsTest";
 
 export default function App() {
   return (
     <>
-      <List />
+      <PropsTest
+        firstName={"철수"}
+        score={{ math: 80, english: 90, history: 100 }}
+      />
     </>
   );
 }
