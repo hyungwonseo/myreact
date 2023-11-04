9e1e11c88a3e1de4d0450a41dfbb63b2e9f3ab18
hwseo12 <hwseo@naver.com>
2023-11-05 02:28:39 +0900

    fix purchase error

--- src/components/GameShopFrontEnd/Purchase.js
+++ src/components/GameShopFrontEnd/Purchase.js
@@ -1,25 +1,27 @@
 import { useQuery } from "react-query";
 import { purchaseGames } from "./api";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export function Purchase() {
   const navigate = useNavigate();
   const location = useLocation();
   const purchasedGames = location.state?.newList;
-  const { data, isLoading } = useQuery("purchase", () => {
+
+  useEffect(() => {
+    const delay = 1000; // 1초 딜레이 시간 (밀리초 단위)
+
     if (purchasedGames && purchasedGames.length > 0) {
       purchaseGames(purchasedGames);
+
+      // 1초 후에 페이지 전환
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, delay);
     } else {
       navigate("/cart");
     }
-  });
-  return (
-    <>
-      {isLoading ? (
-        <h1>구매 처리중.....</h1>
-      ) : (
-        <Navigate to="/dashboard"></Navigate>
-      )}
-    </>
-  );
+  }, []);
+
+  return <h1>구매 처리중.....</h1>;
 }
